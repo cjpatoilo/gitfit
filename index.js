d8f68f74bc124734381a966207a17229479bed10
CJ Patoilo <cjpatoilo@gmail.com>
2018-05-12 10:30:22 -0300

    Fix git branch support

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-const { resolve } = require('path')
-const { execFile, exec } = require('child_process')
+const { exec } = require('child_process')
 const rasper = require('rasper')
-const branch = require('current-git-branch')
+const gitBranch = require('git-branch')
 const { error, log } = console
 const options = process.argv[0].match(/node/i) ? rasper(process.argv.slice(2)) : process.exit(2)
 const command = options._[0]
@@ -33,19 +32,22 @@ if (options.version) {
 	process.exit(2)
 }
 
-if (command.match(/init/)) {
-	execFile('git', ['init', `commit --allow-empty -m 'Initial commit`], err => err ? process.exit(2) : log(`[info] Gitfit initialized`))
+if (command === 'init') {
+	exec(`git init && git commit --allow-empty -m 'Initial commit'`, err => err && error(`[error] Gitfit init command has error`))
+	process.exit(2)
 }
 
-if (command.match(/start/)) {
+if (command === 'start') {
 	if (!argument) error(`[error] Feature name is required\n[info] $ gitfit start <feature-name>`)
-	else execFile('git', [`checkout -b feature/${argument} master`], err => err ? process.exit(2) : log(`[info] Gitfit started feature`))
+	else exec(`git checkout -b feature/${argument} master`, err => err && error(`[error] Gitfit start command has error`))
 	process.exit(2)
 }
 
-if (command.match(/finish/)) {
-	if (!branch.match('feature/')) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
-	else if (!argument) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
-	else execFile('git', ['checkout master', `merge --no-ff feature/${branch}`, `tag -a ${argument}`, `branch -d feature/${branch}`], err => err ? process.exit(2) : log(`[info] Gitfit finished feature`))
-	process.exit(2)
+if (command === 'finish') {
+	gitBranch(branch => {
+		if (!branch.match('feature/')) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
+		else if (!argument) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
+		else exec(`git checkout master && git merge --no-ff feature/${branch} && git tag -a ${argument} && git branch -d feature/${branch}`, err => err && error(`[error] Gitfit finish command has error`))
+		process.exit(2)
+	})
 }
