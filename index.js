ff0365a83b5d1003b294a89bfc2d5bd93b1c8241
CJ Patoilo <cjpatoilo@gmail.com>
2018-05-12 10:00:31 -0300

    Add support to check current branch

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-const { exec } = require('child_process')
+const { resolve } = require('path')
+const { execFile, exec } = require('child_process')
 const rasper = require('rasper')
+const branch = require('current-git-branch')
 const { error, log } = console
 const options = process.argv[0].match(/node/i) ? rasper(process.argv.slice(2)) : process.exit(2)
 const command = options._[0]
 const argument = options._[1]
-const branch = exec(`git branch`)
 
 if (options.help) {
 	log(`
@@ -33,19 +34,18 @@ if (options.version) {
 }
 
 if (command.match(/init/)) {
-	exec(`git init && git commit --allow-empty -m 'Initial commit'`)
-	process.exit(2)
+	execFile('git', ['init', `commit --allow-empty -m 'Initial commit`], err => err ? process.exit(2) : log(`[info] Gitfit initialized`))
 }
 
 if (command.match(/start/)) {
 	if (!argument) error(`[error] Feature name is required\n[info] $ gitfit start <feature-name>`)
-	else exec(`git checkout -b feature/${argument} master'`)
+	else execFile('git', [`checkout -b feature/${argument} master`], err => err ? process.exit(2) : log(`[info] Gitfit started feature`))
 	process.exit(2)
 }
 
 if (command.match(/finish/)) {
 	if (!branch.match('feature/')) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
 	else if (!argument) error(`[error] Tag is required\n[info] $ gitfit finish <new-tag>`)
-	else exec(`git checkout master && git merge --no-ff feature/${branch} && git tag -a ${argument} && git branch -d feature/${branch}'`)
+	else execFile('git', ['checkout master', `merge --no-ff feature/${branch}`, `tag -a ${argument}`, `branch -d feature/${branch}`], err => err ? process.exit(2) : log(`[info] Gitfit finished feature`))
 	process.exit(2)
 }
